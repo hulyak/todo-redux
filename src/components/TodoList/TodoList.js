ee51327835a2829643fd357a36eac8643407843b
hulyak <57311842+hulya95@users.noreply.github.com>
2020-09-20 17:42:13 -0700

    persist the Redux store

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeTodo } from '../../actions';
 import TodoListItem from '../TodoListItem/TodoListItem';
 import NewTodoForm from '../NewTodoForm/NewTodoForm';
 import './TodoList.css';
 
 //  todos props show todo items
-const TodoList = ({ todos = [{ text: 'hello' }] }) => (
+const TodoList = ({ todos = [], onRemovePressed }) => (
   <div className="list-wrapper">
     <NewTodoForm />
-    {todos.map((todo) => <TodoListItem todo={todo} />)}
+    {todos.map((todo, i) => (
+      <TodoListItem key={i} todo={todo} onRemovePressed={onRemovePressed} />
+    ))}
   </div>
 );
 
-export default TodoList;
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onRemovePressed: (text) => dispatch(removeTodo(text)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/store.js
+++ src/store.js
@@ -1,9 +1,21 @@
 import { createStore, combineReducers } from 'redux';
 import { todos } from './reducers';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 const reducers = { todos };
 
 // put reducers in a form that we can pass to the createStore function
 const rootReducer = combineReducers(reducers);
 
-export const configureStore = () => createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const configureStore = () => createStore(rootReducer);
+export const configureStore = () => createStore(persistedReducer);
