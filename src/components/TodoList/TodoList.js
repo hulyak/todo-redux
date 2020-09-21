c1aeda3b4793b18262f019a5580347fc2140235d
hulyak <57311842+hulya95@users.noreply.github.com>
2020-09-20 18:57:34 -0700

    implement reducer for mark as completed button

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { removeTodo } from '../../actions';
+import { removeTodo, markTodoAsCompleted } from '../../actions';
+
 import TodoListItem from '../TodoListItem/TodoListItem';
 import NewTodoForm from '../NewTodoForm/NewTodoForm';
 import './TodoList.css';
 
 //  todos props show todo items
-const TodoList = ({ todos = [], onRemovePressed }) => (
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
   <div className="list-wrapper">
     <NewTodoForm />
     {todos.map((todo, i) => (
-      <TodoListItem key={i} todo={todo} onRemovePressed={onRemovePressed} />
+      <TodoListItem
+        key={i}
+        todo={todo}
+        onRemovePressed={onRemovePressed}
+        onCompletedPressed={onCompletedPressed}
+      />
     ))}
   </div>
 );
@@ -21,6 +27,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   onRemovePressed: (text) => dispatch(removeTodo(text)),
+  onCompletedPressed: (text) => dispatch(markTodoAsCompleted(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
