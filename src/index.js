155d254e42b21eb253f31505d515585c3193d9a5
hulyak <57311842+hulya95@users.noreply.github.com>
2020-09-20 19:40:24 -0700

    add redux thunk to project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
